e820d234da72c9c638ddcbd53acd3a61fdac4792
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2024-12-11 12:40:24 +1100

    Add Redis caching with fallback and cache invalidation

--- backend/src/resolvers/member.resolver.ts
+++ backend/src/resolvers/member.resolver.ts
@@ -1,23 +1,53 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Member } from '../models/member.model';
 import { members } from '../data/members.data';
-import { NotFoundException } from '@nestjs/common';
+import { Logger, NotFoundException } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Redis } from 'ioredis';
+import { MembershipType } from 'src/models/membership.model';
+
+const logger = new Logger('MemberResolver');
 
 @Resolver(() => Member)
 export class MemberResolver {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
   @Query(() => [Member], {
     description: 'Get all members with optional filtering and pagination',
   })
-  getAllMembers(
+  async getAllMembers(
     @Args('membershipType', { type: () => String, nullable: true })
-    membershipType?: string,
+    membershipType = 'ALL',
     @Args('limit', { type: () => Number, nullable: true }) limit = 10,
     @Args('offset', { type: () => Number, nullable: true }) offset = 0,
-  ): Member[] {
-    let filteredMembers = members;
+  ): Promise<Member[]> {
+    this.validateMembershipType(membershipType);
+
+    // First check in the cache and if present, return
+    const cacheKey = `members:${membershipType}:${limit}:${offset}`;
+
+    try {
+      const cachedMembers = await this.redis.get(cacheKey);
+
+      if (cachedMembers) {
+        logger.debug(`Cache hit from cache for key: ${cacheKey}`);
+        return JSON.parse(cachedMembers);
+      }
+
+      logger.log(`Cache miss for key: ${cacheKey}`);
+    } catch (error) {
+      logger.error(`Redis error: ${error.message}. Falling back to database.`);
+    }
+
+    // If not in cache, query the database
+    logger.debug(
+      `Fetching members from database: membershipType=${membershipType}, limit=${limit}, offset=${offset}`,
+    );
+    let filteredMembers = members; // TODO: hardcoded data for now
+    // // logger.debug(`filteredMembers.length: ${filteredMembers.length}`);
 
     // Filter by membership type if provided
-    if (membershipType) {
+    if (membershipType && membershipType !== 'ALL') {
       filteredMembers = filteredMembers.filter(
         (member) =>
           member.membership.membershipType === membershipType.toUpperCase(),
@@ -25,7 +55,26 @@ export class MemberResolver {
     }
 
     // Apply pagination as well
-    return filteredMembers.slice(offset, offset + limit);
+    // // logger.debug(`filteredMembers.length: ${filteredMembers.length}`);
+    const paginatedMembers = filteredMembers.slice(offset, offset + limit);
+
+    // Save to cache with TTL of 60 seconds
+    try {
+      logger.debug(`Trying to Cache members with key: ${cacheKey}`);
+
+      await this.redis.set(
+        cacheKey,
+        JSON.stringify(paginatedMembers),
+        'EX',
+        60,
+      );
+
+      logger.debug(`Successfully Cached members with key: ${cacheKey}`);
+    } catch (error) {
+      logger.debug(`Error Caching members with key: ${cacheKey}`);
+    }
+
+    return paginatedMembers;
   }
 
   @Query(() => Member, { nullable: true, description: 'Get a member by email' })
@@ -37,5 +86,39 @@ export class MemberResolver {
     }
 
     return member;
+  }
+
+  @Mutation(() => Boolean, { description: 'Invalidate members cache' })
+  async invalidateCache(): Promise<boolean> {
+    try {
+      const keys = await this.redis.keys('members:*');
+      if (keys.length > 0) {
+        await this.redis.del(...keys);
+        logger.debug(`Invalidated ${keys.length} keys: ${keys.join(', ')}`);
+      } else {
+        logger.debug('No keys to invalidate.');
+      }
+
+      return true;
+    } catch (error) {
+      logger.error(`Failed to invalidate cache: ${error.message}`);
+      return false;
+    }
+  }
+
+  private validateMembershipType(membershipType: string): void {
+    if (!membershipType || membershipType === 'ALL') {
+      return;
+    }
+
+    if (
+      !Object.values(MembershipType).includes(
+        membershipType.toUpperCase() as MembershipType,
+      )
+    ) {
+      throw new Error(
+        `Invalid membership type: ${membershipType}. Allowed values are GOLD, SILVER, BRONZE.`,
+      );
+    }
   }
 }
