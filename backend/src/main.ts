1c0563ec541cc2ad45b1d4c6bad2e335193bc1d6
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2024-12-11 14:13:24 +1100

    Add Nginx-based load balancing for NestJS servers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
+import { Request, Response } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // add middlewares to log requests
+  app.use((req: Request, res: Response, next: Function) => {
+    if (req.method === 'HEAD' || req.method === 'OPTIONS') {
+      return next(); // Skip logging for these methods
+    }
+
+    console.log(
+      `[NestJS Server: ${PORT}] Handling request: ${req.method} ${req.url}`,
+    );
+
+    next();
+  });
+
+  await app.listen(PORT);
+
+  console.log(`Server running on port ${PORT}`);
 }
 bootstrap();
